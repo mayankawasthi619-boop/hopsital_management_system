c7d1a32acc9e27fb87de8c202c7562dcc37c4aea
myproject-demo <mayankawasthi61975@gmail.com>
2026-03-18 18:18:42 +0530

    Configure GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hopsital_management_system/',
   plugins: [react()],
   server: {
     port: 3000,
